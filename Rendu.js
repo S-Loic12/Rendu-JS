aa985efbe3f4e08d266a2b027add082f2725b93c
S-Loic12 <l.sebera41@gmail.com>
2024-12-04 20:27:03 +0100

    update 1.0

--- Rendu.js
+++ Rendu.js
@@ -6,7 +6,27 @@ fetch('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
         return response.json();
     })
     .then(data => {
-       
+       const produitContainer=document.getElementById('MK1');
+        data.produits.forEach(element => {
+            const card=document.createElement("div")
+            card.className="produit-card";
+            const h2=document.createElement("h2");
+            const descrpt=document.createElement("p");
+            descrpt.textContent=element.description;
+            h2.textContent=element.nom;
+
+
+            
+
+
+            const image=document.createElement("img");
+            image.src=element["image-url"];
+
+            produitContainer.appendChild(card)
+            card.appendChild(h2);
+            card.appendChild(descrpt);
+            card.appendChild(image);
+       });
        console.log(data);
        
     })
