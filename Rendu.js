07469629b4005b68d5d8beb210e9269c6de704df
S-Loic12 <l.sebera41@gmail.com>
2024-12-05 17:04:16 +0100

    Update 2.0

--- Rendu.js
+++ Rendu.js
@@ -8,10 +8,12 @@ fetch('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
     .then(data => {
        const produitContainer=document.getElementById('MK1');
         data.produits.forEach(element => {
-            const card=document.createElement("div")
+            const card =document.createElement("div")
             card.className="produit-card";
             const h2=document.createElement("h2");
             const descrpt=document.createElement("p");
+            h2.className="padding";
+            descrpt.className="padding";
             descrpt.textContent=element.description;
             h2.textContent=element.nom;
 
@@ -23,9 +25,47 @@ fetch('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
             image.src=element["image-url"];
 
             produitContainer.appendChild(card)
+            card.appendChild(image);
             card.appendChild(h2);
             card.appendChild(descrpt);
-            card.appendChild(image);
+           
+       });
+       const serviceContainer=document.getElementById("MK2");
+       data.services.forEach(element=>{
+            const h2=document.createElement("h2");
+            const dept=document.createElement("p");
+            h2.textContent=element.nom;
+            dept.textContent=element.description;
+            h2.className="title";
+            let btn=document.createElement("button");
+            btn.textContent="EN SAVOIR PLUS"
+            btn.className="B1";
+
+            
+            serviceContainer.appendChild(h2);
+            serviceContainer.appendChild(dept);
+            serviceContainer.appendChild(btn);
+       });
+       const temoignagesContainer=document.getElementById("MK3");
+       data.temoignages.forEach(element => {
+            const card=document.createElement("div")
+            card.className="temoignages-card";
+            const prenom=document.createElement("h3");
+            const typeExperience=document.createElement("h4");
+            const commentaire=document.createElement("p");
+            const note=document.createElement("li");
+            
+            prenom.textContent=element.prenom;
+            typeExperience.textContent=element.typeExperience;
+            commentaire.textContent=element.commentaire;
+            note.textContent=element.note;
+        
+           temoignagesContainer.appendChild(card);
+           card.appendChild(prenom);
+           card.appendChild(typeExperience);
+           card.appendChild(commentaire);
+           card.appendChild(note);
+
        });
        console.log(data);
        
